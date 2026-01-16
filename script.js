04ac79a4a19c89755b0af9cc9a8bc782ed874124
Biswajit Bala <iambiswajitbala.dev@gmail.com>
2026-01-16 23:28:39 +0530

    feat: update the index

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+$(document).ready(function () {
+    // Smooth scrolling for details links
+    $('a.nav-link, .btn').on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 70 // Adjust for navbar height
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
 
+    // Navbar active state on scroll
+    $(window).scroll(function () {
+        var scrollDistance = $(window).scrollTop();
+        $('section').each(function (i) {
+            if ($(this).position().top - 100 <= scrollDistance) {
+                $('.navbar-nav a.active').removeClass('active');
+                $('.navbar-nav a').eq(i).addClass('active');
+            }
+        });
+
+        // Scroll top button
+        if ($(this).scrollTop() > 300) {
+            $('#scrollTop').fadeIn();
+        } else {
+            $('#scrollTop').fadeOut();
+        }
+    });
+
+    $('#scrollTop').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+});
